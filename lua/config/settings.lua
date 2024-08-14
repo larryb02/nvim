e78ef31eb5fa4404ab049dc93fc566c2047b18f8
Larry Banks <larrykbanks21@gmail.com>
2024-08-14 11:44:54 -0400

    config

---
+++ lua/config/settings.lua
@@ -0,0 +1,14 @@
+-- Enable filetype detection, plugins, and indentation
+vim.cmd('filetype plugin indent on')
+
+-- Set tab width to 4 spaces
+vim.opt.tabstop = 4
+
+-- Set shift width to 4 spaces
+vim.opt.shiftwidth = 4
+
+-- Use spaces instead of tabs
+vim.opt.expandtab = true
+vim.cmd.syntax('on')
+vim.cmd.set('number')
+vim.cmd.set('cursorline')
