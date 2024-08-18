d9797bdc3c250beaf7a9c8aded247461f4d11d4c
Larry Banks <larrykbanks21@gmail.com>
2024-08-18 17:19:04 -0400

    more stuff

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 require('config.settings')
 require('config.mappings')
 require('config.lazy')
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,35 +1,72 @@
 return
 {
     {
-        "williamboman/mason.nvim",
-        lazy = false,
-        opts = {}
-    },
-
-    {
-        "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        opts = {
-            ensure_installed = {
-                "clangd",
-                "lua_ls",
-                -- "pyright",
-                -- "yamlls"
-            }
-        }
-    },
-
-    {
         "neovim/nvim-lspconfig",
-        config = function(_, servers)
-            servers = {
-                clangd = {},
-                lua_ls = {},
-            }
-            for server, opts in pairs(servers) do
-                require("lspconfig")[server].setup(opts)
-            end
-        end,
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/nvim-cmp",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+        },
+
+        config = function()
+            local cmp = require('cmp')
+            -- local cmp_lsp = require('cmp_nvim_lsp')
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "clangd",
+                    "lua_ls",
+                    "ansiblels",
+                    "pyright",
+                },
+
+                handlers = {
+                    function(server_name)
+                        require("lspconfig")[server_name].setup{}
+                    end,
+                    ["lua_ls"] = function()
+                        local lspconfig = require("lspconfig")
+                        lspconfig.lua_ls.setup {
+                            settings = {
+                                Lua = {
+                                    diagnostics = {
+                                        globals = { "vim", },
+                                    }
+                                }
+                            }
+                        }
+                    end,
+                }
+            })
+
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                }, {
+                    { name = 'buffer' },
+                })
+            })
+        end
     },
 
     {
@@ -38,5 +75,38 @@ return
         lazy = false,
         opts = {}
 
-    }
+    },
+
+    {
+        "tris203/precognition.nvim",
+        opts = {},
+    },
+
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {},
+    },
+
+    -- {
+        --     "nvim-telescope/telescope.nvim",
+        --     branch = '0.1.x',
+        --     keys = {},
+        --     dependencies = {
+            --         "nvim-lua/plenary.nvim",
+            --         -- "BurntSushi/ripgrep",
+            --     },
+            --     opts = {}
+            -- },
+
+            -- {
+                --     "nvim-tree/nvim-tree.lua",
+                --     dependencies = {
+                    --         "nvim-tree/nvim-web-devicons"
+                    --     },
+                    --     lazy = false,
+                    --     opts = {},
+                    --     enable = false
+                    -- },
+
 }
